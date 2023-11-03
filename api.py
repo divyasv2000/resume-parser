77d1ba904a9cd3032281d373162f38ac2f1595d2
divyasv2000 <divyamangalpelli25@gmail.com>
2023-11-04 01:20:52 +0530

    added authentication key

--- api.py
+++ api.py
@@ -1,20 +1,22 @@
 from typing import Union
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, status, Header
 from new import get_json
 import os
 
-from fastapi import FastAPI, File, UploadFile
-
 app = FastAPI()
 
+SECRET_API_KEY = "resumeapi@2023"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Naruto"}
-
+def api_key_dependency(api_key: str = Header(...)):
+    if api_key != SECRET_API_KEY:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
+    return api_key
 
 @app.post("/upload")
-def upload_file(file: UploadFile = File()):
-    # try:
+def upload_file(file: UploadFile = File(), api_key: str = Header(..., convert_underscores=False, description="API Key")):
+    if api_key != SECRET_API_KEY:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
+    
     contents = file.file.read()
     with open("uploads/"+file.filename, 'wb') as f:
         f.write(contents)
